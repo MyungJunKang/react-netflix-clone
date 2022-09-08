c31deb77a3a249cc49f53c37c8dc4b05086741b4
iocrops-MyungJun <myungjun@iocrops-MyungJunui-MacBookPro.local>
2022-09-08 16:43:46 +0900

    feat: add video banner

--- src/App/Components/Banner/Banner.js
+++ src/App/Components/Banner/Banner.js
@@ -5,9 +5,10 @@ import { getNowPlaying, getMovieDetail } from "../../Services/MovieService";
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
-
+  const [isClick, setIsClick] = useState(false);
   const fetchData = async () => {
     const getMovie = await getNowPlaying();
+    console.log(getMovie);
     const movieId =
       getMovie.data.results[
         Math.floor(Math.random() * getMovie.data.results.length)
@@ -31,19 +32,47 @@ const Banner = () => {
         backgroundSize: "cover",
       }}
     >
-      <div className={styles.bannerContents}>
-        <h1 className={styles.bannerTitle}>
-          {movie.title || movie.name || movie.original}
-        </h1>
-        <div className={styles.bannerButton}>
-          <button className={cn(styles.btn, styles.play)}>Play</button>
-          <button className={cn(styles.btn, styles.info)}>Info</button>
+      {!isClick ? (
+        <div className={styles.bannerContents}>
+          <h1 className={styles.bannerTitle}>
+            {movie.title || movie.name || movie.original}
+          </h1>
+          <div className={styles.bannerButton}>
+            <button
+              className={cn(styles.btn, styles.play)}
+              onClick={() => setIsClick(true)}
+            >
+              Play
+            </button>
+            <button className={cn(styles.btn, styles.info)}>Info</button>
+          </div>
+          <div className={styles.bannerDescription}>
+            {truncate(movie.overview, 100)}
+          </div>
+          <div className={styles.bannerFadeBottom} />
         </div>
-        <div className={styles.bannerDescription}>
-          {truncate(movie.overview, 100)}
+      ) : (
+        <div className={styles.videoWrapper}>
+          <div className={styles.container}>
+            {movie.videos.results[0].key ? (
+              <>
+                <iframe
+                  id={movie.id}
+                  title={movie.title || movie.name || movie.original}
+                  className={styles.iframe}
+                  width="560"
+                  height="315"
+                  src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+                  allow="autoplay;"
+                  allowFullScreen
+                ></iframe>
+              </>
+            ) : (
+              <div>비디오가 없습니다</div>
+            )}
+          </div>
         </div>
-        <div className={styles.bannerFadeBottom} />
-      </div>
+      )}
     </div>
   );
 };
